f5376c61220915fb360d5d21f394ae30622af69b
Davit <davit@Air-Davit.lan>
2019-06-16 19:57:32 +0400

    Learn more despatch

--- src/index.js
+++ src/index.js
@@ -1,34 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { browserHistory, Router,Route } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux'; 
-import { Provider } from 'react-redux';
-import reducers from 'reducers';
-//import Components
-import Layout from 'containers/layout';
-import Phones from 'containers/phones';
-
 import './main.css'
 
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import {syncHistoryWithStore} from 'react-router-redux'
+import {Router, Route, browserHistory} from 'react-router'
+import {Provider} from 'react-redux'
+
+import reducers from 'reducers'
+import Layout from 'containers/layout'
+import Phones from 'containers/phones'
 
 const store = createStore(reducers, composeWithDevTools(
     applyMiddleware(thunk)
-));
+))
 
-const history = syncHistoryWithStore(browserHistory, store);
+const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
     <Provider store={store}>
         <Router history={history}>
             <Route component={Layout}>
-                <Route path="/" component={Phones} />
+                <Route path='/' component={Phones} />
             </Route>
         </Router>
-    </Provider>,    
+    </Provider>,
     document.getElementById('root')
-)
-
-
+);
