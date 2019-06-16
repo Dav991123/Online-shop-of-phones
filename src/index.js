fa17151f81766aa4726663ef9eeca0be26effb30
Davit <davit@Air-Davit.lan>
2019-06-16 14:48:45 +0400

    import setings

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { browserHistory, Router,Route } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux'; 
+import { Provider } from 'react-redux';
+import reducers from 'reducers';
+//import Components
+import Layout from 'containers/layout';
+import Phones from 'containers/phones';
+
 import './main.css'
 
-document.write('Hello React/Redux!')
+
+const store = createStore(reducers, composeWithDevTools(
+    applyMiddleware(thunk)
+));
+
+const history = syncHistoryWithStore(browserHistory, store);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={history}>
+            <Route component={Layout}>
+                <Route path="/" component={Phones} />
+            </Route>
+        </Router>
+    </Provider>,    
+    document.getElementById('root')
+)
+
+
